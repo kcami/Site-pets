c106104cd1cf02e21461c1b54e38e9ab51f21673
Camila M Reno <camilamreno@gmail.com>
2022-02-23 15:51:56 -0300

    Adicionando página de cuidados ao adotar

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,24 @@
-console.log('teste');
+$(document).ready(function() {
+    setTimeout(function(){
+
+        $('#id-titulo').parallax({imageSrc: 'img/pet-cuidado-1.jpg'});
+
+    }, 250);
+
+    // Iniciando o loader quando o usuário chega no elemento
+    let dataAteaOffset = $('#id-cachorro').offset();
+    let stop = 0; // variavel para parar o evento de scroll quando acontece uma vez
+
+    $(window).scroll(function(e) {
+        let scroll = $(window).scrollTop();
+
+        if(scroll > (dataAteaOffset.top - 500) && stop == 0) {
+            circleA.animate(1.0);
+            circleB.animate(1.0);
+            circleC.animate(1.0);
+            circleD.animate(1.0);
+
+            stop=1;
+        }
+    });
+});
