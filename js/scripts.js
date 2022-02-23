363510999343c9693701009dbb1617f0a1de6e67
caio <caioantunes@byronsolutions.com>
2022-02-23 02:33:12 -0300

    fazendo ajustes necessarios nas paginas

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,11 @@
-console.log('teste');
+$(document).ready(function() {
+ // Parallax
+
+  // setTimeout serve para carregar primeiro as imagens
+  setTimeout(function() {
+    $('#data-area').parallax({imageSrc: 'img/cidadeparallax.png'});
+    $('#apply-area').parallax({imageSrc: 'img/pattern.png'});
+  }, 200);
+}
+
+
