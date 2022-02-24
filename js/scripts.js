f5f730b4c9da06ba52d39f378bad24613c75a0ab
Camila M Reno <camilamreno@gmail.com>
2022-02-24 00:18:14 -0300

    Adicionando animação no botão e arrumando o parallax

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     setTimeout(function(){
 
         $('#id-titulo').parallax({imageSrc: 'img/pet-cuidado-1.jpg'});
-        $('#apply-area').parallax({imageSrc: 'img/pet-cuidado-1.jpg'});
+        $('#pet-img').parallax({imageSrc: 'img/gatorro.jfif'});
 
     }, 200);
 });
