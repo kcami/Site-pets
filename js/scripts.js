bd2c67c42afdcf96bea50ef46e7eb53338bd56dc
Camila M Reno <camilamreno@gmail.com>
2022-02-24 18:04:50 -0300

    Auto stash before merge of "master" and "origin/develop"
    Arrumando conflitos

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,7 @@ $(document).ready(function() {
     setTimeout(function(){
 
         $('#id-titulo').parallax({imageSrc: 'img/pet-cuidado-1.jpg'});
-<<<<<<< HEAD
         $('#id-sobre').parallax({imageSrc: 'img/sobre-unifei.jpg'});
-=======
->>>>>>> f5f730b4c9da06ba52d39f378bad24613c75a0ab
         $('#pet-img').parallax({imageSrc: 'img/gatorro.jfif'});
 
     }, 200);
