7deba957abf7e17ff14dccfa2f8b96879f82dd75
Camila M Reno <camilamreno@gmail.com>
2022-02-25 11:04:43 -0300

    Adicionando parallax e responsivo em algumas páginas

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,7 @@ $(document).ready(function() {
         $('#id-titulo').parallax({imageSrc: 'img/pet-cuidado-1.jpg'});
         $('#id-sobre').parallax({imageSrc: 'img/sobre-unifei.jpg'});
         $('#pet-img').parallax({imageSrc: 'img/gatorro.jfif'});
+        $('#id-adocao').parallax({imageSrc: 'img/adocao-pet.jpg'});
 
     }, 200);
 });
